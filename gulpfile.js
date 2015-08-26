0dcc3944ed35e5dd86dea03e0797991d944651ea
CI-Server <ci@example.com>
2015-08-25 20:20:23 -0700

    Got publishing working

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 'use strict';
 
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
 gulp.task('copy', function () {
   return gulp.src('app/**')
     .pipe(gulp.dest('dist'));
+});
+
+var ghPagesOptions = {
+    message: 'Update ' + new Date().toISOString() + ' [skip ci]',
+    branch: 'gh-pages'
+};
+
+gulp.task('default', ['copy']);
+
+gulp.task('deploy', ['default'], function () {
+  return gulp.src('dist/**')
+    .pipe($.ghPages(ghPagesOptions));
 });
